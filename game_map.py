d2d475485ad05189718912be51f4b532bdd24395
voynix <voynix26@gmail.com>
2021-04-19 23:19:43 -0400

    tcod tutorial part 3

--- game_map.py
+++ game_map.py
@@ -7,9 +7,7 @@ import tile_types
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
-        self.tiles = np.full((width, height), fill_value=tile_types.FLOOR, order='F')
-
-        self.tiles[30:33, 22] = tile_types.WALL
+        self.tiles = np.full((width, height), fill_value=tile_types.WALL, order='F')
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside the bounds of this map"""
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 
 TITLE = 'KOBOLD-LIKE'
@@ -11,6 +11,10 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
+
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
 
 
 def main() -> None:
@@ -25,7 +29,14 @@ def main() -> None:
     npc = Entity(SCREEN_WIDTH // 2 - 5, SCREEN_HEIGHT // 2, '@', (255, 255, 0))
     entities = {npc, player}
 
-    game_map = GameMap(MAP_WIDTH, MAP_HEIGHT)
+    game_map = generate_dungeon(
+        max_rooms=MAX_ROOMS,
+        room_min_size=ROOM_MIN_SIZE,
+        room_max_size=ROOM_MAX_SIZE,
+        map_width=MAP_WIDTH,
+        map_height=MAP_HEIGHT,
+        player=player
+    )
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
